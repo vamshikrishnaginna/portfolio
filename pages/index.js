612a2a30540e03daf5dc112dd6a04a570b4a50c3
Ashfaq <ashfaqnisar00@gmail.com>
2021-07-31 17:38:09 +0530

    feat: updated the index page

--- pages/index.js
+++ pages/index.js
@@ -2,73 +2,21 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-draculaPrimaryBG">
       <Head>
-        <title>Create Next App</title>
+        <title>Ashfaq Nisar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className=" text-4xl text-red-500 dark:text-yellow-600">
-          Ashfaq <a className="text-blue">Nisar</a>
+        <h1 className="text-3xl text-white">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I'm <span className={"text-5xl text-buffy"}>Ashfaq Nisar </span>
         </h1>
 
-        <p className="mt-3 text-2xl text-gray-600">Hello There!</p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className="mt-3 text-xl text-gray-200">
+          Greetings, Welcome to the beautiful site!
+        </p>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      darker: "#282a36",
+      dark: "#44475a",
+      light: "#f8f8f2",
+      blue: "#6272a4",
+      cyan: "#8be9fd",
+      green: "#50fa7b",
+      orange: "#ffb86c",
+      pink: "#ff79c6",
+      purple: "#bd93f9",
+      red: "#ff5555",
+      yellow: "#f1fa8c",
+      nosferatu: "#282a36",
+      aro: "#44475a",
+      cullen: "#f8f8f2",
+      vonCount: "#6272a4",
+      vanHelsing: "#8be9fd",
+      blade: "#50fa7b",
+      morbius: "#ffb86c",
+      buffy: "#ff79c6",
+      dracula: "#bd93f9",
+      marcelin: "#ff5555",
+      lincoln: "#f1fa8c",
+      draculaPrimaryBG: "#383a59",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
