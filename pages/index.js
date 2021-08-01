f8388d30e49fdc8a8aeb1bef63cee23f6847d91b
Ashfaq <ashfaqnisar00@gmail.com>
2021-08-01 15:51:23 +0530

    feat: Changed the direction of the components

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,32 @@ import profilePicture from "../public/images/profile.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-draculaPrimaryBG">
+    <>
+      {/*<div className=" items-center justify-center min-h-screen py-2 bg-draculaPrimaryBG">*/}
       <Head>
         <title>Ashfaq Nisar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className={"w-64"}>
-          <Image src={profilePicture} alt={"Picture of Ashfaq"} />
-        </div>
-        <h1 className="mt-3 text-3xl text-white">
-          {/* eslint-disable-next-line react/no-unescaped-entities */}
-          I'm <span className={"text-5xl text-buffy"}>Ashfaq Nisar </span>
-        </h1>
+      <main className={"min-h-screen bg-draculaPrimaryBG"}>
+        <div className="md:container md:mx-auto">
+          <div className="h-screen flex flex-row items-center justify-evenly">
+            <div>
+              <h1 className="text-3xl text-white">
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                I'm <span className={"text-5xl text-buffy"}>Ashfaq Nisar </span>
+              </h1>
+              <p className="mt-3 text-xl text-gray-200">
+                Hi, You can find me in a corner doing my own thing.
+              </p>
+            </div>
 
-        <p className="mt-3 text-xl text-gray-200">
-          Hi, You can find me in a corner doing my own thing.
-        </p>
+            <div className="w-72">
+              <Image src={profilePicture} alt={"Picture of Ashfaq"} />
+            </div>
+          </div>
+        </div>
       </main>
-    </div>
+      {/*</div>*/}
+    </>
   );
 }
