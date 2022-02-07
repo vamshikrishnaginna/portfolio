1772e3e332b586341a7da625ec0c4fb9625fb93d
Ashfaq <ashfaqnisar00@gmail.com>
2022-02-07 13:09:29 +0530

    feat: Added the pageview count

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import TopBar from "./components/TopBar";
+import { useRouter } from "next/router";
+import { pageview } from "../../utils/analytics";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className={"flex flex-col h-screen"}>
       <TopBar />
